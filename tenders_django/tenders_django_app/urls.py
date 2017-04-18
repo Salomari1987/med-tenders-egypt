00c0929f9e78e02a80a118d1f49b6cef661695ae
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-04-18 23:41:32 +0300

    Design homepage

--- tenders_django/tenders_django_app/urls.py
+++ tenders_django/tenders_django_app/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from tenders_django_app.views import TendersView
+from django.views.generic import TemplateView
+
 
 
 urlpatterns = {
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^tenders/$', TendersView.as_view(), name="tenders"),
 }
